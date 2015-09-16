bf8b4402ae6ce39f8091e263d042161f6a13ef1c
cisnb125 <cisnb125@gmail.com>
2015-09-16 10:17:31 +0900

    Added data.json for Firebase import.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,77 +1,70 @@
 angular.module('mychat.controllers', [])
 
-.controller('LoginCtrl', function ($scope, $ionicModal, $state, $firebaseAuth, $ionicLoading, $rootScope) {
-    //console.log('Login Controller Initialized');
+  .controller('LoginCtrl', function ($scope, $ionicModal, $state, $firebaseAuth, $ionicLoading, $rootScope) {
+    console.log('Login Controller Initialized firebaseUrl:', firebaseUrl);
 
-    var ref = new Firebase($scope.firebaseUrl);
+    var ref = new Firebase(firebaseUrl);
     var auth = $firebaseAuth(ref);
 
     $ionicModal.fromTemplateUrl('templates/signup.html', {
-        scope: $scope
+      scope: $scope
     }).then(function (modal) {
-        $scope.modal = modal;
+      $scope.modal = modal;
     });
 
     $scope.createUser = function (user) {
-        console.log("Create User Function called");
-        if (user && user.email && user.password && user.displayname) {
-            $ionicLoading.show({
-                template: 'Signing Up...'
-            });
+      console.log("Create User Function called");
+      if (user && user.name) {
+        $ionicLoading.show({
+          template: 'Signing Up...'
+        });
 
-            auth.$createUser({
-                email: user.email,
-                password: user.password
-            }).then(function (userData) {
-                alert("User created successfully!");
-                ref.child("users").child(userData.uid).set({
-                    email: user.email,
-                    displayName: user.displayname
-                });
-                $ionicLoading.hide();
-                $scope.modal.hide();
-            }).catch(function (error) {
-                alert("Error: " + error);
-                $ionicLoading.hide();
+        auth.$authAnonymously()
+          .then(function (userData) {
+            alert("User created successfully!");
+            ref.child("users").child(userData.uid).set({
+              name: user.email
             });
-        } else
-            alert("Please fill all details");
-    }
+            $ionicLoading.hide();
+            $scope.modal.hide();
+          }).catch(function (error) {
+            alert("Error: " + error);
+            $ionicLoading.hide();
+          });
+      } else
+        alert("Please fill all details");
+    };
 
     $scope.signIn = function (user) {
 
-        if (user && user.email && user.pwdForLogin) {
-            $ionicLoading.show({
-                template: 'Signing In...'
-            });
-            auth.$authWithPassword({
-                email: user.email,
-                password: user.pwdForLogin
-            }).then(function (authData) {
-                console.log("Logged in as:" + authData.uid);
-                ref.child("users").child(authData.uid).once('value', function (snapshot) {
-                    var val = snapshot.val();
-                    // To Update AngularJS $scope either use $apply or $timeout
-                    $scope.$apply(function () {
-                        $rootScope.displayName = val;
-                    });
-                });
-                $ionicLoading.hide();
-                $state.go('tab.rooms');
-            }).catch(function (error) {
-                alert("Authentication failed:" + error.message);
-                $ionicLoading.hide();
-            });
-        } else
-            alert("Please enter email and password both");
+      if (user && user.displayName) {
+        $ionicLoading.show({
+          template: 'Signing In...'
+        });
+        auth.$authAnonymously({
+          displayName: user.displayName
+        }).then(function (authData) {
+          console.log("Logged in as:" + authData.uid);
+          ref.child("users").child(authData.uid).set({
+            displayName: user.displayName
+          });
+          $rootScope.displayName = user.displayName;
+          $ionicLoading.hide();
+          $state.go('tab.rooms');
+        }).catch(function (error) {
+          alert("Authentication failed:" + error.message);
+          $ionicLoading.hide();
+        });
+      } else
+        alert("유저네임이 어떻게 되시죠?");
     }
-})
+  })
 
-.controller('ChatCtrl', function ($scope, Chats, $state) {
-    //console.log("Chat Controller initialized");
+  .controller('ChatCtrl', function ($scope, Chats, $state) {
+    console.log("Chat Controller initialized");
 
     $scope.IM = {
-        textMessage: ""
+      textMessage: ""
     };
 
     Chats.selectRoom($state.params.roomId);
@@ -80,28 +73,28 @@ angular.module('mychat.controllers', [])
 
     // Fetching Chat Records only if a Room is Selected
     if (roomName) {
-        $scope.roomName = " - " + roomName;
-        $scope.chats = Chats.all();
+      $scope.roomName = " - " + roomName;
+      $scope.chats = Chats.all();
     }
 
     $scope.sendMessage = function (msg) {
-        console.log(msg);
-        Chats.send($scope.displayName, msg);
-        $scope.IM.textMessage = "";
+      console.log($scope.displayName, msg);
+      Chats.send($scope.displayName, msg);
+      $scope.IM.textMessage = "";
     }
 
     $scope.remove = function (chat) {
-        Chats.remove(chat);
+      Chats.remove(chat);
     }
-})
+  })
 
-.controller('RoomsCtrl', function ($scope, Rooms, Chats, $state) {
+  .controller('RoomsCtrl', function ($scope, Rooms, Chats, $state) {
     //console.log("Rooms Controller initialized");
     $scope.rooms = Rooms.all();
 
     $scope.openChatRoom = function (roomId) {
-        $state.go('tab.chat', {
-            roomId: roomId
-        });
+      $state.go('tab.chat', {
+        roomId: roomId
+      });
     }
-});
+  });
